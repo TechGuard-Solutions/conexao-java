f2dfeae49e2a7ea50c0c1ad30651155b206c2123
Gustavo Gil <gustavo.gil@sptech.school>
2024-10-27 04:13:11 -0300

    crontab

--- start.sh
+++ start.sh
@@ -2,5 +2,8 @@
 # Iniciar o serviço cron
 service cron start
 
+echo "0 16 * * * java -jar target/Integracao-1.0-SNAPSHOT-jar-with-dependencies.jar" > /etc/cron.d/mycron
+crontab /etc/cron.d/mycron
+
 # Iniciar a aplicação Java
 java -jar target/Integracao-1.0-SNAPSHOT-jar-with-dependencies.jar
