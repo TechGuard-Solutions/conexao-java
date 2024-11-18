820f35953dd341f3144bc919a41a5c2399acb715
GuiSousa2910 <122701467+GuiSousa2910@users.noreply.github.com>
2024-11-18 18:07:52 -0300

    testenaod

--- src/main/java/org/techguard/Principal.java
+++ src/main/java/org/techguard/Principal.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import io.github.cdimascio.dotenv.Dotenv;
 
 public class Principal {
     private static final Logger LOGGER = LogManager.getLogger(Principal.class);
